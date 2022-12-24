d3622329f5d83910162fa2038669f3a5bbfffc7f
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-24 19:03:22 -0300

    chore: tailwind

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1 class="text-4xl bg-blue-500">Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
