f757038a0cd463bad9158a3d3f742c112c2df80e
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-24 17:47:16 -0300

    chore: add testing library

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    // ...
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
   },
 })
 
